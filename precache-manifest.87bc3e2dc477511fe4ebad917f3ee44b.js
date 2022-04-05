7d5a0ff89f39e483f5114bdd8e3f2d429dffc1c7
kpgreene131 <kpgreene131@gmail.com>
2022-04-05 19:58:01 -0400

    Updates

---
+++ precache-manifest.87bc3e2dc477511fe4ebad917f3ee44b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "04fbf35ae319e510213d",
+    "url": "/static/css/main.42419857.chunk.css"
+  },
+  {
+    "revision": "04fbf35ae319e510213d",
+    "url": "/static/js/main.04fbf35a.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "/static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "ab6d1c5f064ab3950bc7",
+    "url": "/static/css/2.c19d2415.chunk.css"
+  },
+  {
+    "revision": "ab6d1c5f064ab3950bc7",
+    "url": "/static/js/2.ab6d1c5f.chunk.js"
+  },
+  {
+    "revision": "ca15752fc42e5c2f840d",
+    "url": "/static/js/runtime~main.ca15752f.js"
+  },
+  {
+    "revision": "2bcb46f5488430f845a55c4e741b4cc0",
+    "url": "/static/media/logo.2bcb46f5.png"
+  },
+  {
+    "revision": "4d8c1785422743272c5b331238bb6d74",
+    "url": "/static/media/nails1.4d8c1785.JPEG"
+  },
+  {
+    "revision": "21bdaf4515c7eb57ed26fe0297888302",
+    "url": "/static/media/nails2.21bdaf45.JPEG"
+  },
+  {
+    "revision": "82e969ac0167f9e5aaf8906847dc1447",
+    "url": "/static/media/nails3.82e969ac.JPEG"
+  },
+  {
+    "revision": "7338c569cbd529458285ae2f79d328d3",
+    "url": "/static/media/nails4.7338c569.JPEG"
+  },
+  {
+    "revision": "80876067f100290fab181a7cba55d959",
+    "url": "/static/media/nails5.80876067.JPEG"
+  },
+  {
+    "revision": "e98278f10d8312f43e497955da4989d0",
+    "url": "/static/media/nails6.e98278f1.JPEG"
+  },
+  {
+    "revision": "95c9ea7aafb89e7d1e34c9701c631d8a",
+    "url": "/index.html"
+  }
+];
