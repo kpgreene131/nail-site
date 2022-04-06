2546adb92081e8357abbaff97dd8965a1712316d
kpgreene131 <kpgreene131@gmail.com>
2022-04-05 20:00:38 -0400

    comitting manifest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Contact from './components/Contact';
@@ -9,14 +9,14 @@ function App() {
   return (
     <div className='pageContainer'>
       <Router>
-      <Navbar />
-      <div className = "contentWrapper">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+        <Navbar />
+        <div className = "contentWrapper">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/Contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
